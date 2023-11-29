1666547c84c742f23264f21cec1bc288b590cab1
marcos.rebello <marcos.rebello@RS-DS07>
2023-11-29 15:48:36 -0300

    ok

--- cria_exe.bat
+++ cria_exe.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\7-Zip-Zstandard\7z.exe" a HotAPP_Modelo.7z HotAPP_Modelo.zip
+"%~dp0\7z.exe" a HotAPP_Modelo.7z HotAPP_Modelo.zip
 copy /b 7zsd_All_x64.sfx + config.txt + HotAPP_Modelo.7z HotAPP_Modelo_install.exe
