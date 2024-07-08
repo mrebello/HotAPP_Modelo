5c27d7fc32709928bf7f0262959292d889e95520
Marcos Paulo Ferreira Rebello <rebello@mensa.org.br>
2024-07-08 15:11:33 -0300

    FluentUI funcionando

--- HotAPP_Modelo/Program.cs
+++ HotAPP_Modelo/Program.cs
@@ -1,7 +1,14 @@
 using HotAPP_Modelo.Data;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 namespace HotAPP_Modelo;
+
+
+/// <summary>
+/// ATENÇÃO: Os assets não são copiados automaticamente.
+/// Para gerar os assets atualizados e copiar, realizar uma publicação e copiar a pasta wwwroot.
+/// </summary>
+
 
 internal class Program : HotAPP<App> {
     [STAThread]
@@ -11,6 +18,8 @@ internal class Program : HotAPP<App> {
 
     public override void Config_Services(IServiceCollection services) {
         base.Config_Services(services);
+
+        services.AddFluentUIComponents();
 
         services.AddSingleton<WeatherForecastService>();
     }
